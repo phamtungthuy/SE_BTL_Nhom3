7dfa96d17c3ee520276cb4bcb0505aed29fee602
phamtungthuy <thuy200347@gmail.com>
2023-03-21 10:20:08 +0700

    Connect to db using mysql, sequelize library

--- backend/src/config/viewEngine.js
+++ backend/src/config/viewEngine.js
@@ -1,6 +1,24 @@
 import express from 'express';
+import Sequelize from 'sequelize';
+require('dotenv').config();
 
-let conFigViewEngine = (app) => {
+
+const sequelize = new Sequelize(process.env.DATABASE_NAME, 'root', process.env.DATABASE_PASSWORD, {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: process.env.DATABASE_PORT
+});
+
+
+
+
+let conFigViewEngine = async (app) => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
     app.use(express.static("./src/public"));
     app.set("view engine", "ejs");
     app.set("views", "./src/views");
