42ce7c92cb97a792c53d27b9ef6785c8f9b2468b
phamtungthuy <thuy200347@gmail.com>
2023-03-23 10:18:45 +0700

    move connectDB to new file connectDB.js seperate to manage easier

--- backend/src/config/connectDB.js
+++ backend/src/config/connectDB.js
@@ -1,19 +1,24 @@
-import Sequelize from 'sequelize';
-require('dotenv').config();
+import Sequelize from "sequelize";
+require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DATABASE_NAME, 'root', process.env.DATABASE_PASSWORD, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: process.env.DATABASE_PORT
-  });
-
-  let connectDB = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-      }
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME,
+  "root",
+  process.env.DATABASE_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    port: process.env.DATABASE_PORT,
   }
+);
+
+let connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
 
 module.exports = connectDB;
